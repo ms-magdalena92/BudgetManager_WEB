047ba62ecdc7b119c38c40a5b3e677d8bafbde50
Magda S <magdalena.slomiany.programista@gmail.com>
2020-03-26 19:19:14 +0100

    Created add expense page

--- budget.js
+++ budget.js
@@ -1,10 +1,25 @@
 function showPassword()
 {
-  var password = document.getElementById("passwordInput");
-  if (password.type === "password") {
-	password.type = "text";
-  }
-  else {
-	password.type = "password";
-  }
+	 var password = document.getElementById("passwordInput");
+	 if (password.type === "password")
+		password.type = "text";
+	 else
+		password.type = "password";
+}
+
+function getCurrentDate()
+{
+	var date = new Date();
+
+	var day = date.getDate();
+	var month = date.getMonth() + 1;
+	var year = date.getFullYear();
+
+	if (month < 10)
+		month = "0" + month;
+	if (day < 10)
+		day = "0" + day;
+
+	var today = year + "-" + month + "-" + day;       
+	document.getElementById("dateInput").value = today;
 }

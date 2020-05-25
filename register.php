b127add6e2fda452d4c0c0db73c4e380d23c0f49
Magda S <magdalena.slomiany.programista@gmail.com>
2020-05-25 16:38:19 +0200

    Moved js file to js folder

--- register.php
+++ register.php
@@ -288,7 +288,7 @@
 	</footer>
 	
 	<script src="js/bootstrap.min.js"></script>
-	<script src="budget.js"></script>
+	<script src="js/budget.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	
 </body>
